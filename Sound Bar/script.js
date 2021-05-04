c562b28930edbd19bbd2ab68afefeabcdf72a681
Abhishek <abhishek96.r@gmail.com>
2021-05-04 16:57:34 +0530

    Initial Commit

---
+++ Sound Bar/script.js
@@ -0,0 +1,25 @@
+const sounds = ['crash', 'kick-bass', 'snare', 'tom-1', 'tom-2', 'tom-3', 'tom-4']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText =sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime=0;
+    })
+}
